e9a9413a122adc1f44e004d749487e4ef255d4f4
Yuxuan Liu <1145356576@qq.com>
2025-06-04 23:42:58 -0700

    update visualization for prose-fd

--- src/scripts/0.sh
+++ src/scripts/0.sh
@@ -1,6 +1,13 @@
 GPU=0
 GPUs=0,1
 
-expid=prose_ft_8
-torchrun --standalone --nnodes 1 --nproc_per_node 4 main.py use_wandb=0 exp_name=prose_fd exp_id=${expid} batch_size=16 data=turb compile=1 optim=adamw optim.scheduler_type=cosine optim.lr=1e-4 save_periodic=4 max_epoch=1 reload_model=checkpoint/prose_fd/prose_20/best-_l2_error.pth n_steps_per_epoch=2000 model=prose_2to1 symbol.symbol_input=1 &&
-torchrun --standalone --nnodes 1 --nproc_per_node 4 main.py eval_only=1 use_wandb=0 exp_name=prose_fd_eval eval_from_exp=checkpoint/prose_fd/${expid} log_eval_plots=-1 exp_id=${expid} batch_size_eval=64 data=turb model=prose_2to1 symbol.symbol_input=1 
+# expid=prose_20 # muon, prose, qk norm
+# python main.py eval_only=1 use_wandb=0 exp_name=debug eval_from_exp=checkpoint/prose_fd/${expid} log_eval_plots=-1 exp_id=${expid} batch_size_eval=50 model=prose_2to1 symbol.symbol_input=1 data=turb data.com_ns.type=all
+
+
+
+# expid=bcat_muon_all_11 # main exp, 10% warmup
+# torchrun --standalone --nnodes 1 --nproc_per_node 4 main.py eval_only=1 use_wandb=0 exp_name=debug eval_from_exp=checkpoint/fluids_test/${expid} log_eval_plots=-1 exp_id=${expid} batch_size_eval=64 data=turb data.com_ns.type=all
+
+expid=prose_5 # adam, prose, qk norm
+python main.py eval_only=1 use_wandb=0 exp_name=save eval_from_exp=checkpoint/prose_fd/${expid} log_eval_plots=-1 exp_id=${expid} batch_size_eval=50 model=prose_2to1 symbol.symbol_input=1 save_outputs=1 eval_size=1
