03ce78791c60c29c3a7f7665a4bc2f139bfd3f11
Yuxuan Liu <1145356576@qq.com>
2025-10-01 21:08:08 -0700

    update for fno

--- src/scripts/0.sh
+++ src/scripts/0.sh
@@ -1,13 +1,6 @@
-GPU=0
+GPU=1
 GPUs=0,1
 
-# expid=prose_20 # muon, prose, qk norm
-# python main.py eval_only=1 use_wandb=0 exp_name=debug eval_from_exp=checkpoint/prose_fd/${expid} log_eval_plots=-1 exp_id=${expid} batch_size_eval=50 model=prose_2to1 symbol.symbol_input=1 data=turb data.com_ns.type=all
-
-
-
-# expid=bcat_muon_all_11 # main exp, 10% warmup
-# torchrun --standalone --nnodes 1 --nproc_per_node 4 main.py eval_only=1 use_wandb=0 exp_name=debug eval_from_exp=checkpoint/fluids_test/${expid} log_eval_plots=-1 exp_id=${expid} batch_size_eval=64 data=turb data.com_ns.type=all
-
-expid=prose_5 # adam, prose, qk norm
-python main.py eval_only=1 use_wandb=0 exp_name=save eval_from_exp=checkpoint/prose_fd/${expid} log_eval_plots=-1 exp_id=${expid} batch_size_eval=50 model=prose_2to1 symbol.symbol_input=1 save_outputs=1 eval_size=1
+expid=fno_test_3
+# CUDA_VISIBLE_DEVICES=$GPU python main.py exp_id=${expid} batch_size=64 model=fno amp=0 max_epoch=20 &&
+CUDA_VISIBLE_DEVICES=$GPU python main.py eval_only=1 use_wandb=0 exp_name=ts_eval eval_from_exp=checkpoint/ts/${expid} log_eval_plots=-1 exp_id=${expid} batch_size_eval=128 model=fno amp=0
