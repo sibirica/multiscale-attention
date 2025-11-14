9a90525c57c60bc348b8e59131c1d8663232ba14
Daniel Gurevich <nerpa12@gmail.com>
2025-11-14 02:28:43 +0000

    initial commit

---
+++ src/scripts/bcat_test.sh
@@ -0,0 +1,5 @@
+# run only BCAT without ablation tests
+expid=bcat
+python main.py exp_id=${expid} batch_size=16 data=fluids_arena compile=1 optim=muon model.flex_attn=1 #dryrun=1
+#torchrun --standalone --nnodes 1 --nproc_per_node 4 main.py exp_id=${expid} batch_size=32 data=fluids_sample compile=1 optim=muon model.flex_attn=1
+#torchrun --standalone --nnodes 1 --nproc_per_node 4 main.py eval_only=1 use_wandb=0 exp_name=eval eval_from_exp=checkpoint/bcat/${expid} log_eval_plots=-1 exp_id=${expid} batch_size_eval=64 model.flex_attn=1
